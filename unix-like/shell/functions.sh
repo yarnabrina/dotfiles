d51d34f59ac79de1378c71635386d8fee7400350
Anirban Ray <39331844+yarnabrina@users.noreply.github.com>
2023-03-26 21:03:23 +0530

    moved pre-commit helper function

--- unix-like/shell/functions.sh
+++ unix-like/shell/functions.sh
@@ -4,7 +4,6 @@ FUNCTIONS_DIRECTORY="${HOME}/dotfiles/unix-like/shell/functions"
 source "${FUNCTIONS_DIRECTORY}"/compare-files.sh
 source "${FUNCTIONS_DIRECTORY}"/others.sh
 source "${FUNCTIONS_DIRECTORY}"/pip-helpers.sh
-source "${FUNCTIONS_DIRECTORY}"/pre-commit-helpers.sh
 source "${FUNCTIONS_DIRECTORY}"/python-helpers.sh
 
 # teardown
--- unix-like/shell/functions/others.sh
+++ unix-like/shell/functions/others.sh
@@ -43,3 +43,14 @@ function venv() {
         source ./venv/bin/activate
     fi
 }
+
+# run pre-commit hooks on all files
+function pcra() {
+    if [[ "${#}" -eq 0 ]]; then
+        echo "Running all pre-commit hooks on all files in the repository"
+        pre-commit run --all-files
+    else
+        echo "Running "${1}" pre-commit hook on all files in the repository"
+        pre-commit run --all-files "${1}"
+    fi
+}
